f7ea79c57f5ae6343fac3f0d3a58ceeab6ced9f2
Marc Hartsuiker <marc.hartsuiker@gmail.com>
2015-11-21 16:46:04 +0100

    updated server.js

--- server.js
+++ server.js
@@ -9,9 +9,8 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public/scripts'));  
     
 //It will find and locate index.html from View or Scripts
-//app.get('/',function(req,res){
-  //res.sendFile('index.html');
-//});
+app.get("/", function(req, res) {
+13   res.sendfile("index.html", { root: __dirname });
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + process.env.PORT);
