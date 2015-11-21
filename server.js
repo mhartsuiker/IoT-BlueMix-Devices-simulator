27105da00a2c1f09e21add8d34ca06aa01ec2bce
Marc Hartsuiker <marc.hartsuiker@gmail.com>
2015-11-21 15:50:12 +0100

    removed deprecated code

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ var favicon = require('serve-favicon');
 var app     = express();
 
 // Configuration
-app.configure(function() {
-    app.set('port', process.env.PORT || 8080); 
-    app.use(favicon(__dirname + '/public/favicon.ico'));    
-    app.use(express.static(__dirname + '/public'));
-    app.use(express.static(__dirname + '/public/scripts'));   
-});
-
+app.set('port', process.env.PORT || 8080); 
+app.use(favicon(__dirname + '/public/favicon.ico'));    
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/scripts'));  
+    
 //It will find and locate index.html from View or Scripts
 app.get('/',function(req,res){
   res.sendFile('index.html');
